1d5997eeae97c49ea64a03b017055d7ea2e2ed8f
Marco2a94 <marco.arrighi@efrei.net>
2023-09-24 23:51:07 -0700

    First commit including static endpoints

---
+++ start.js
@@ -0,0 +1,20 @@
+let express = require("express");
+let port = process.env.PORT || 5000;
+
+let app = express();
+let path = require('path');
+/*
+app.get("/", (req, res) => {
+    res.status(200).json({message: "Welcome on my API in NodeJS"});
+});*/
+
+
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.use((req, res) => {
+    res.status(404).send('<H1>Error 404 : Ressource not found !');
+});
+
+app.listen(port, () => {
+    console.log("Server started succesfully");
+});
